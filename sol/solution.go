4ad079f52b72f2227f9c12763a7ca7d970a7d7b8
jsonLiang <yuany90221@gmail.com>
2021-07-16 23:55:26 +0800

    without array

--- sol/solution.go
+++ sol/solution.go
@@ -1,14 +1,17 @@
 package sol
 
 func climbStairs(n int) int {
-	// create space to store result
-	ways := make([]int, n)
-	// step 1
-	ways[0] = 1
-	ways[1] = 2
-
-	for stairs := 3; stairs <= n; stairs++ {
-		ways[stairs-1] = ways[stairs-2] + ways[stairs-3]
+	if n <= 3 {
+		return n
 	}
-	return ways[n-1]
+	ways := 0
+	prevTwo := 2
+	prevOne := 3
+	for stairs := 3; stairs <= n; stairs++ {
+		ways = prevTwo + prevOne
+		prevTwo = prevOne
+		prevOne = ways
+	}
+
+	return ways
 }
